d7c8896b0a6ad8332b7fb887623e3e5a9a2a488f
Vlad Durnev <leopold.dva@gmail.com>
2018-02-15 00:45:26 +0300

    refactoring for screens

--- core/src/com/crawn/game/screens/GameScreen.java
+++ core/src/com/crawn/game/screens/GameScreen.java
@@ -8,7 +8,7 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.utils.TimeUtils;
 import com.badlogic.gdx.utils.Timer;
 import com.crawn.game.Crawn;
-import com.crawn.game.model.PlayAccount;
+import com.crawn.game.model.accounts.AccountManager;
 import com.crawn.game.utils.resource.manager.ResourceManager;
 import com.crawn.game.widgets.GameWidgetGroup;
 
@@ -17,12 +17,12 @@ final public class GameScreen implements Screen {
     GameScreen() {
         batch = ((Crawn) Gdx.app.getApplicationListener()).getMainBatch();
         background = new Sprite((Texture) ResourceManager.instance().get("textures/bg.jpg"));
-        account = new PlayAccount("awesome acc", 0 , 0);
+        accountManager = new AccountManager();
     }
 
     @Override
     public void show() {
-        ((Crawn) Gdx.app.getApplicationListener()).getStage().addActor(new GameWidgetGroup(account));
+        ((Crawn) Gdx.app.getApplicationListener()).getStage().addActor(new GameWidgetGroup(accountManager));
     }
 
     @Override
@@ -59,8 +59,8 @@ final public class GameScreen implements Screen {
 
 
     private long timerDelay;
-    private PlayAccount account;
-
+    final private AccountManager accountManager;
+    
     final private Sprite background;
     final private Batch batch;
 }
--- core/src/com/crawn/game/screens/LoadingScreen.java
+++ core/src/com/crawn/game/screens/LoadingScreen.java
@@ -3,6 +3,8 @@ package com.crawn.game.screens;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ai.btree.BehaviorTree;
+import com.badlogic.gdx.ai.btree.utils.BehaviorTreeLibraryManager;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -17,11 +19,12 @@ final public class LoadingScreen implements Screen {
         batch = ((Crawn) Gdx.app.getApplicationListener()).getMainBatch();
         ResourceManager.instance().loadFile("game_skin/game_widget_skin.json", false);
         ResourceManager.instance().loadSection("audio", false);
-        ResourceManager.instance().loadSection("account_userdata", false);
         ResourceManager.instance().loadSection("i18n", false);
         ResourceManager.instance().loadSection("textures", false);
+        ResourceManager.instance().loadSection("behavior_tree", false);
 
-        progressBar = new ProgressBar(0, 100, 1, true, (Skin) ResourceManager.instance().get("preload_skin/preload_skin.json"));
+        final Skin preloadSkin = (Skin) ResourceManager.instance().get("preload_skin/preload_skin.json");
+        progressBar = new ProgressBar(0, 100, 1, true, preloadSkin);
         progressBar.setSize(progressBar.getWidth(), Gdx.graphics.getHeight() * 0.8f);
 
         final float x = Gdx.graphics.getWidth() - Gdx.graphics.getWidth() * 0.1f;
@@ -39,6 +42,10 @@ final public class LoadingScreen implements Screen {
 
         if (progress == 100) {
             MusicManager.instance().initialize();
+            BehaviorTreeLibraryManager.getInstance().getLibrary().registerArchetypeTree(
+                    "villainAI",
+                    (BehaviorTree) ResourceManager.instance().get("behavior_tree/villain.btree")
+            );
             ((Game) Gdx.app.getApplicationListener()).setScreen(new MainMenuScreen());
         }
     }
