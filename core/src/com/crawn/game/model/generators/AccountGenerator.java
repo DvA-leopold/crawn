7d50881fac01afcc1b859e2aff4e2b05aabeabc9
Vlad Durnev <leopold.dva@gmail.com>
2018-02-15 00:46:27 +0300

    add account generator skeleton

--- core/src/com/crawn/game/model/generators/AccountGenerator.java
+++ core/src/com/crawn/game/model/generators/AccountGenerator.java
@@ -1,17 +1,17 @@
 package com.crawn.game.model.generators;
 
-import com.crawn.game.model.PlayAccount;
+import com.crawn.game.model.accounts.VillainAccount;
 
 import java.util.Vector;
 
 
 final public class AccountGenerator {
-    private static PlayAccount generateAccount() {
-        return new PlayAccount(RandomNamesGenerator.generateAccountName(), 0, 0);
+    private static VillainAccount generateAccount() {
+        return new VillainAccount(RandomNamesGenerator.generateAccountName(), 0, 0, 0);
     }
 
-    public static Vector<PlayAccount> generateAccounts(int accountNumber) {
-        Vector<PlayAccount> playAccountVector = new Vector<>(accountNumber);
+    public static Vector<VillainAccount> generateAccounts(int accountNumber) {
+        final Vector<VillainAccount> playAccountVector = new Vector<>(accountNumber);
         for (int i = 0; i < accountNumber; i++) {
             playAccountVector.add(generateAccount());
         }
